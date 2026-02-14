5e54009c9586f005202b0000e6d92896449b4b9f
ChanchalRawate <102480285+ChanchalRawate@users.noreply.github.com>
2026-02-14 12:29:38 +0530

    Delete src/load_data.py

--- src/load_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel("../data/USAforeignworkerssalarydata.csv")
-
-print("Shape:", df.shape)
-print("\nColumns:\n", df.columns)
-print("\nMissing values:\n", df.isnull().sum())
